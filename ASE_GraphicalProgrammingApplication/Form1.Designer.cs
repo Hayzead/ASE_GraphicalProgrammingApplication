f57c180becd88a8f108141d2dd35226902ac19d2
azkolapo1@gmail.com <azkolapo1@gmail.com>
2021-11-18 12:25:39 +0000

    updated Ui form design

--- ASE_GraphicalProgrammingApplication/Form1.Designer.cs
+++ ASE_GraphicalProgrammingApplication/Form1.Designer.cs
@@ -37,6 +37,7 @@ namespace ASE_GraphicalProgrammingApplication
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -105,11 +106,21 @@ namespace ASE_GraphicalProgrammingApplication
             this.saveToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
             this.saveToolStripMenuItem.Text = "Save";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(866, 959);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(293, 54);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Syntax Checking";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1888, 1025);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -135,6 +146,7 @@ namespace ASE_GraphicalProgrammingApplication
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
     }
 }
 
